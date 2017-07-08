82ca989efbe6396d5392f09f77e6e27257bf1c3f
julianopoveda <julianopoveda@gmail.com>
2017-07-08 16:16:45 -0300

    Bullet e GameManager Behavior adaptadas

--- PlanaltoDefense/Assets/GameManagerBehavior.cs
+++ PlanaltoDefense/Assets/GameManagerBehavior.cs
@@ -1,39 +1,215 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class GameManagerBehavior : MonoBehaviour {
+public class GameManagerBehavior : MonoBehaviour
+{
 
-	public Text goldLabel;
-	private int gold;
+    public Text goldLabel;
+
+    public Text GoldLabel
+    {
+        get
+        {
+            try
+            {
+                return goldLabel;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        set
+        {
+            try
+            {
+                goldLabel = value;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+
+    private int gold;
+
     public Text waveLabel;
+
+    public Text WaveLabel
+    {
+        get
+        {
+            try
+            {
+                return waveLabel;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        set
+        {
+            try
+            {
+                waveLabel = value;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+
     public GameObject[] nextWaveLabels;
+
+    public GameObject[] NextWaveLabels
+    {
+        get
+        {
+            try
+            {
+                return nextWaveLabels;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        set
+        {
+            try
+            {
+                nextWaveLabels = value;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+
     public Text healthLabel;
+
+    public Text HealthLabel
+    {
+        get
+        {
+            try
+            {
+                return healthLabel;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        set
+        {
+            try
+            {
+                healthLabel = value;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+
     public GameObject[] healthIndicator;
-    public bool gameOver = false;
+
+    public GameObject[] HealthIndicator
+    {
+        get
+        {
+            try
+            {
+                return healthIndicator;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        set
+        {
+            try
+            {
+                healthIndicator = value;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+
+    public bool gameOver;
+
+    public bool GameOver
+    {
+        get
+        {
+            try
+            {
+                return gameOver;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        set
+        {
+            try
+            {
+                gameOver = value;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+
     private int wave;
     private int health;
 
+    public GameManagerBehavior()
+    {
+        GameOver = false;
+    }
+
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         Wave = 0;
         Gold = 500;
-        Health = 5; 
+        Health = 5;
     }
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 
-	public int Gold {
-		get { return gold; }
-		set {
-			gold = value;
-			goldLabel.GetComponent<Text>().text = "Seus pontos: " + gold ;
-		}
-	}
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public int Gold
+    {
+        get { return gold; }
+        set
+        {
+            gold = value;
+            goldLabel.GetComponent<Text>().text = "Seus pontos: " + gold;
+        }
+    }
 
     public int Wave
     {
