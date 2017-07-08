d62d1d5046f1f6ff903b16f4ace0db414323d073
julianopoveda <julianopoveda@gmail.com>
2017-07-08 15:04:11 -0300

    Classes de Monster com propriedades

--- PlanaltoDefense/Assets/MonsterData.cs
+++ PlanaltoDefense/Assets/MonsterData.cs
@@ -1,73 +1,225 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 [System.Serializable]
-public class MonsterLevel {
-	public int cost;
-	public GameObject visualization;
+public class MonsterLevel
+{
+    public int cost;
+
+    public int Cost
+    {
+        get
+        {
+            try
+            {
+                return cost;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        set
+        {
+            try
+            {
+                cost = value;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+
+    public GameObject visualization;
+
+    public GameObject Visualization
+    {
+        get
+        {
+            try
+            {
+                return visualization;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        set
+        {
+            try
+            {
+                visualization = value;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+
     public GameObject bullet;
+
+    public GameObject Bullet
+    {
+        get
+        {
+            try
+            {
+                return bullet;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        set
+        {
+            try
+            {
+                bullet = value;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+
     public float fireRate;
+
+    public float FireRate
+    {
+        get
+        {
+            try
+            {
+                return fireRate;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        set
+        {
+            try
+            {
+                fireRate = value;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
 }
 
-public class MonsterData : MonoBehaviour {
+public class MonsterData : MonoBehaviour
+{
+    public List<MonsterLevel> levels;
 
-	public List<MonsterLevel> levels;
-	private MonsterLevel currentLevel;
+    public List<MonsterLevel> Levels {
+        get
+        {
+            try
+            {
+                return levels;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        set
+        {
+            try
+            {
+                levels = value;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private MonsterLevel currentLevel;
 
-	void OnEnable() {
-		CurrentLevel = levels[0];
-	}
+    // Use this for initialization
+    void Start()
+    {
 
-	//1
-	public MonsterLevel CurrentLevel {
-		//2
-		get {
-			return currentLevel;
-		}
-		//3
-		set {
-			currentLevel = value;
-			int currentLevelIndex = levels.IndexOf(currentLevel);
+    }
 
-			GameObject levelVisualization = levels[currentLevelIndex].visualization;
-			for (int i = 0; i < levels.Count; i++) {
-				if (levelVisualization != null) {
-					if (i == currentLevelIndex) {
-						levels[i].visualization.SetActive(true);
-					} else {
-						levels[i].visualization.SetActive(false);
-					}
-				}
-			}
-		}
-	}
+    // Update is called once per frame
+    void Update()
+    {
 
-	public MonsterLevel getNextLevel() {
-		int currentLevelIndex = levels.IndexOf (currentLevel);
-		int maxLevelIndex = levels.Count - 1;
-		if (currentLevelIndex < maxLevelIndex) {
-			return levels[currentLevelIndex+1];
-		} else {
-			return null;
-		}
-	}
+    }
 
-	public void increaseLevel() {
-		int currentLevelIndex = levels.IndexOf(currentLevel);
-		if (currentLevelIndex < levels.Count - 1) {
-			CurrentLevel = levels[currentLevelIndex + 1];
-		}
-	}
+    void OnEnable()
+    {
+        CurrentLevel = levels[0];
+    }
+
+    //1
+    public MonsterLevel CurrentLevel
+    {
+        //2
+        get
+        {
+            return currentLevel;
+        }
+        //3
+        set
+        {
+            currentLevel = value;
+            int currentLevelIndex = levels.IndexOf(currentLevel);
+
+            GameObject levelVisualization = levels[currentLevelIndex].visualization;
+            for (int i = 0; i < levels.Count; i++)
+            {
+                if (levelVisualization != null)
+                {
+                    if (i == currentLevelIndex)
+                    {
+                        levels[i].visualization.SetActive(true);
+                    }
+                    else
+                    {
+                        levels[i].visualization.SetActive(false);
+                    }
+                }
+            }
+        }
+    }
+
+    public MonsterLevel getNextLevel()
+    {
+        int currentLevelIndex = levels.IndexOf(currentLevel);
+        int maxLevelIndex = levels.Count - 1;
+        if (currentLevelIndex < maxLevelIndex)
+        {
+            return levels[currentLevelIndex + 1];
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    public void increaseLevel()
+    {
+        int currentLevelIndex = levels.IndexOf(currentLevel);
+        if (currentLevelIndex < levels.Count - 1)
+        {
+            CurrentLevel = levels[currentLevelIndex + 1];
+        }
+    }
 
 }
--- PlanaltoDefense/Assets/PlaceMonster.cs
+++ PlanaltoDefense/Assets/PlaceMonster.cs
@@ -1,60 +1,152 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlaceMonster : MonoBehaviour {
+public class PlaceMonster : MonoBehaviour
+{
+    public GameObject monsterPrefab;
 
-	public GameObject monsterPrefab;
-	private GameObject monster;
-	private GameManagerBehavior gameManager;
+    public GameObject MonsterPrefab
+    {
+        get
+        {
+            try
+            {
+                return monsterPrefab;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        set
+        {
+            try
+            {
+                monsterPrefab = value;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
 
-	// Use this for initialization
-	void Start () {
-		gameManager = GameObject.Find("GameManager").GetComponent<GameManagerBehavior> ();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private GameObject monster;
 
-	private bool canPlaceMonster() {
+    private GameObject Monster
+    {
+        get
+        {
+            try
+            {
+                return monster;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        set
+        {
+            try
+            {
+                monster = value;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+
+    private GameManagerBehavior gameManager;
+
+    private GameManagerBehavior GameManager
+    {
+        get
+        {
+            try
+            {
+                return gameManager;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        set
+        {
+            try
+            {
+                gameManager = value;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManagerBehavior>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private bool canPlaceMonster()
+    {
         //MonsterData monsterData = monster.GetComponent<MonsterData>();
         return (monster == null && gameManager.Gold >= 200);
-	}
+    }
 
-	//1
-	void OnMouseUp () {
-		//2
-		if (canPlaceMonster () ) {
-			//3
-			monster = (GameObject) 
-				Instantiate(monsterPrefab, transform.position, Quaternion.identity);
-			//4
-			AudioSource audioSource = gameObject.GetComponent<AudioSource>();
-			audioSource.PlayOneShot(audioSource.clip);
+    //1
+    void OnMouseUp()
+    {
+        //2
+        if (canPlaceMonster())
+        {
+            //3
+            monster = (GameObject)
+                Instantiate(monsterPrefab, transform.position, Quaternion.identity);
+            //4
+            AudioSource audioSource = gameObject.GetComponent<AudioSource>();
+            audioSource.PlayOneShot(audioSource.clip);
 
-			gameManager.Gold -= monster.GetComponent<MonsterData>().CurrentLevel.cost;
+            gameManager.Gold -= monster.GetComponent<MonsterData>().CurrentLevel.cost;
 
-		} else if (canUpgradeMonster()) {
-			monster.GetComponent<MonsterData>().increaseLevel();
-			AudioSource audioSource = gameObject.GetComponent<AudioSource>();
-			audioSource.PlayOneShot(audioSource.clip);
+        }
+        else if (canUpgradeMonster())
+        {
+            monster.GetComponent<MonsterData>().increaseLevel();
+            AudioSource audioSource = gameObject.GetComponent<AudioSource>();
+            audioSource.PlayOneShot(audioSource.clip);
 
-			gameManager.Gold -= monster.GetComponent<MonsterData>().CurrentLevel.cost;
+            gameManager.Gold -= monster.GetComponent<MonsterData>().CurrentLevel.cost;
 
-		}
-	}
+        }
+    }
 
-	private bool canUpgradeMonster() {
-		if (monster != null) {
-			MonsterData monsterData = monster.GetComponent<MonsterData> ();
-			MonsterLevel nextLevel = monsterData.getNextLevel();
-			if (nextLevel != null) {
-				return gameManager.Gold >= nextLevel.cost;
-			}
-		}
-		return false;
-	}
+    private bool canUpgradeMonster()
+    {
+        if (monster != null)
+        {
+            MonsterData monsterData = monster.GetComponent<MonsterData>();
+            MonsterLevel nextLevel = monsterData.getNextLevel();
+            if (nextLevel != null)
+            {
+                return gameManager.Gold >= nextLevel.cost;
+            }
+        }
+        return false;
+    }
 
 }
